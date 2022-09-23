3cab49714a41a24a7ce58a9cc66f5cce8c7777d7
mofeideyu <mofei2914376031@163.com>
2022-09-23 08:34:45 +0800

    join方法描述

--- src/main/java/com/mashibing/juc/c_000/T03_Sleep_Yield_Join.java
+++ src/main/java/com/mashibing/juc/c_000/T03_Sleep_Yield_Join.java
@@ -25,7 +25,9 @@ public class T03_Sleep_Yield_Join {
         new Thread(()->{
             for(int i=0; i<100; i++) {
                 System.out.println("A" + i);
-                if(i%10 == 0) Thread.yield();
+                if(i%10 == 0) {
+                    Thread.yield();
+                }
 
 
             }
@@ -34,11 +36,18 @@ public class T03_Sleep_Yield_Join {
         new Thread(()->{
             for(int i=0; i<100; i++) {
                 System.out.println("------------B" + i);
-                if(i%10 == 0) Thread.yield();
+                if(i%10 == 0) {
+                    Thread.yield();
+                }
             }
         }).start();
     }
 
+    /**
+     * 线程同步
+     * 如有线程t1和t2；在线程t2内加入线程t1.join(),两者可以先后随即运行,仅包括t1.join()方法
+     * 之前的代码，当t2线程运行到t1.join()时,就会等待t1线程执行完毕,t2线程才会继续执行。
+     */
     static void testJoin() {
         Thread t1 = new Thread(()->{
             for(int i=0; i<100; i++) {
